f09198366bf28badb8342064e842b62ad878d747
eluinstra <eluinstra@luin.dev>
2021-08-08 21:06:44 +0200

    improvements

--- src/main/java/dev/luin/digikoppeling/gb/server/service/Url.java
+++ src/main/java/dev/luin/digikoppeling/gb/server/service/Url.java
@@ -1,25 +1,33 @@
 package dev.luin.digikoppeling.gb.server.service;
 
+import static dev.luin.file.server.core.ValueObject.isNotNull;
+
 import java.net.URL;
 
 import dev.luin.file.server.core.ValueObject;
 import dev.luin.file.server.core.file.VirtualPath;
 import io.vavr.control.Try;
 import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
 import lombok.NonNull;
+import lombok.ToString;
 import lombok.experimental.FieldDefaults;
 
 //@Value
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@EqualsAndHashCode
+@ToString
 public class Url implements ValueObject<String>
 {
 	URL value;
 
 	public Url(String url)
 	{
-		this(Try.success(url.contains("://0.0.0.0:") ? url.replace("://0.0.0.0:","://localhost:") : url)
-				.mapTry(u -> new URL(u))
-				.getOrElseThrow(() -> new IllegalArgumentException("Url is invalid")));
+		this(Try.success(url)
+				.flatMap(isNotNull())
+				.map(u -> u.contains("://0.0.0.0:") ? u.replace("://0.0.0.0:","://localhost:") : u)
+				.mapTry(URL::new)
+				.getOrElseThrow(e -> new IllegalArgumentException("Url is invalid",e)));
 	}
 
 	public Url(URL url)
