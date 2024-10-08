a6992b8fc768929a1948427b6fa9630f3b966700
eluinstra <eluinstra@luin.dev>
2024-10-08 22:29:46 +0200

    added spi

--- src/main/java/dev/luin/digikoppeling/gb/server/service/GBServiceConfig.java
+++ src/main/java/dev/luin/digikoppeling/gb/server/service/GBServiceConfig.java
@@ -16,24 +16,40 @@
 package dev.luin.digikoppeling.gb.server.service;
 
 import dev.luin.file.server.core.file.FileSystem;
+import dev.luin.file.server.web.WebConfig;
+import jakarta.xml.ws.Endpoint;
+
 import javax.xml.datatype.DatatypeConfigurationException;
 import lombok.AccessLevel;
 import lombok.experimental.FieldDefaults;
+
+import org.apache.cxf.endpoint.Server;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+@PropertySource(value = {"classpath:dev/luin/digikoppeling/gb/server/default.properties"}, ignoreResourceNotFound = true)
 @FieldDefaults(level = AccessLevel.PRIVATE)
-public class GBServiceConfig
+public class GBServiceConfig extends WebConfig
 {
-	@Value("${server.baseUrl}")
-	String baseUrl;
-
 	@Bean
-	public GBService gbService(@Autowired FileSystem fileSystem) throws DatatypeConfigurationException
+	public GBService gbService(@Autowired FileSystem fileSystem, @Value("${server.baseUrl}") String baseUrl) throws DatatypeConfigurationException
 	{
 		return new GBServiceImpl(fileSystem, new ExternalDataReferenceBuilder(new Url(baseUrl + "/download")));
+	}
+
+	@Bean
+	public Endpoint gbServiceEndpoint(GBService gbService)
+	{
+		return publishEndpoint(gbService, "/gb", "http://luin.dev/digikoppeling/gb/server/1.0", "GBService", "GBServicePort");
+	}
+
+	@Bean
+	public Server createGBJAXRSServer(GBService gbService)
+	{
+		return createJAXRSServer(GBServiceImpl.class, gbService, "/gb");
 	}
 }
